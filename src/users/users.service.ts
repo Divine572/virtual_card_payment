a37e36d4a6bd384a334adab081a195b666a1ba17
Adewambe Daniel <danoscolobo@gmail.com>
2023-02-16 01:15:00 +0100

    fixed customer creation server error

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,173 +14,218 @@ import MongoError from 'src/utils/mongoError.enum';
 
 @Injectable()
 export class UsersService {
-    constructor(@InjectModel(User.name) private userModel: Model<UserDocument>, private configService: ConfigService) {
-    }
+    constructor(
+        @InjectModel(User.name) private userModel: Model<UserDocument>,
+        private configService: ConfigService,
+    ) {}
 
-    TOKEN = this.configService.get('NODE_ENV') === 'production'? this.configService.get('SUDO_API_KEY'): this.configService.get('SUDO_API_DEV_KEY')
-   
+    TOKEN =
+        this.configService.get('NODE_ENV') === 'production'
+            ? this.configService.get('SUDO_API_KEY')
+            : this.configService.get('SUDO_API_DEV_KEY');
+
     headers = {
-        'accept': 'application/json',
-        "Authorization": `Bearer ${this.TOKEN}`,
+        accept: 'application/json',
+        Authorization: `Bearer ${this.TOKEN}`,
         'content-type': 'application/json',
-    }
+    };
 
-
-
+    sudoApi = axios.create({
+        baseURL:
+            this.configService.get('NODE_ENV') === 'development'
+                ? `${this.configService.get('SUDO_BASE_TEST_URL')}`
+                : `${this.configService.get('SUDO_BASE_URL')}`,
+        headers: this.headers,
+    });
 
     async get() {
-        return this.userModel.find()
+        return this.userModel.find();
     }
 
     async getByEmail(email: string) {
-        const user = await this.userModel.findOne({ email })
+        const user = await this.userModel.findOne({ email });
 
         if (!user) {
-            throw new HttpException('User with this email does not exist', HttpStatus.NOT_FOUND);
+            throw new HttpException(
+                'User with this email does not exist',
+                HttpStatus.NOT_FOUND,
+            );
         }
 
-        return user
+        return user;
     }
 
-    
-
     async getBySudoId(sudoID: string) {
-        const user = await this.userModel.findOne({ sudoID: sudoID})
+        const user = await this.userModel.findOne({ sudoID: sudoID });
 
         if (!user) {
-            throw new HttpException('User with this id does not exist', HttpStatus.NOT_FOUND);
+            throw new HttpException(
+                'User with this id does not exist',
+                HttpStatus.NOT_FOUND,
+            );
         }
 
-        return user
+        return user;
     }
 
     async createUser(userData: CreateUserDto) {
         try {
-            const hashedPassword = await bcrypt.hash(userData.password, 10)
+            const hashedPassword = await bcrypt.hash(userData.password, 10);
 
-            const url = this.configService.get('NODE_ENV') === 'development' ? `${this.configService.get('SUDO_BASE_TEST_URL')}/customers`: `${this.configService.get('SUDO_BASE_URL')}/customers`
-            const individualData = {
-                type: userData.customerType,
-                name: userData.fullName,
-                phoneNumber: userData.phoneNumber,
-                emailAddress: userData.email,
-                status: 'active',
-                billingAddress: {
-                    line1: userData.address,
-                    city: userData.city,
-                    state: userData.state,
-                    postalCode: userData.postalCode,
-                    country: userData.country
-                },
-                individual: {
-                    firstName: userData.firstName,
-                    lastName: userData.lastName,
-                    dob: userData.dob,
-                    identity: {
-                        type: userData.identityType,
-                        number: userData.identityNumber
-                    }
-                }
-            }
-
-            const companyData = {
-                type: userData.customerType,
-                name: userData.fullName,
-                phoneNumber: userData.phoneNumber,
-                emailAddress: userData.email,
-                status: 'active',
-                billingAddress: {
-                    line1: userData.address,
-                    city: userData.city,
-                    state: userData.state,
-                    postalCode: userData.postalCode,
-                    country: userData.country
-                },
-                company: {
-                    name: userData.companyName,
-                    identity: {
-                        type: userData.identityType,
-                        number: userData.identityNumber
-                    }
-                }
-            }
-
-            let data = {}
-
+            const url =
+                this.configService.get('NODE_ENV') === 'development'
+                    ? `${this.configService.get(
+                          'SUDO_BASE_TEST_URL',
+                      )}/customers`
+                    : `${this.configService.get('SUDO_BASE_URL')}/customers`;
+            // individual
+            const transformedPhoneNumber = userData.phoneNumber.replace(
+                /^0/,
+                '+234',
+            );
             if (userData.customerType === CustomerType.INDIVIDUAL) {
-                data = {
-                    ...individualData
+                const sudoCustomer = await this.sudoApi({
+                    method: 'POST',
+                    url: '/customers',
+                    data: {
+                        type: 'individual',
+                        individual: {
+                            identity: { type: 'BVN', number: userData.bvn },
+                            firstName: userData.firstName,
+                            lastName: userData.lastName,
+                            dob: userData.dob,
+                        },
+                        status: 'active',
+                        billingAddress: {
+                            line1: userData.address1,
+                            city: userData.city,
+                            state: userData.state,
+                            postalCode: userData.postalCode,
+                            country: userData.country,
+                        },
+                        name: userData.fullName,
+                        phoneNumber: userData.phoneNumber,
+                        emailAddress: userData.email,
+                    },
+                    //     {
+                    //         "type": "individual",
+                    //         "individual": {
+                    //              "identity": {
+                    //                   "type": "BVN",
+                    //                   "number": "22912474187"
+                    //              },
+                    //              "firstName": "Bill",
+                    //              "lastName": "Cosby",
+                    //              "dob": "1999/11/12"
+                    //         },
+                    //         "status": "active",
+                    //         "billingAddress": {
+                    //              "line1": "9, Billy road",
+                    //              "city": "Lagos",
+                    //              "state": "Lagos",
+                    //              "postalCode": "100242",
+                    //              "country": "Nigeria"
+                    //         },
+                    //         "name": "Bill Cosby",
+                    //         "phoneNumber": "7033519240",
+                    //         "emailAddress": "bill2@example.com"
+                    //    }
+                });
+
+                if (sudoCustomer.status !== 201) {
+                    console.log(sudoCustomer);
+                    throw new HttpException(
+                        {
+                            code: 'INTERNAL_SERVER_ERROR',
+                            message: 'Something went wrong',
+                        },
+                        HttpStatus.INTERNAL_SERVER_ERROR,
+                    );
                 }
-            } else if (userData.identityType === CustomerType.COMPANY) {
-                data = {
-                    ...companyData
+
+                const sudoCustomerWallet = await this.sudoApi({
+                    method: 'POST',
+                    url: '/accounts',
+                    data: {
+                        currency: 'NGN',
+                        type: 'wallet',
+                        accountType: 'Current',
+                        customerId: sudoCustomer.data.data._id,
+                    },
+                });
+
+                if (sudoCustomerWallet.status !== 200) {
+                    throw new HttpException(
+                        {
+                            code: 'INTERNAL_SERVER_ERROR',
+                            message: 'Something went wrong',
+                        },
+                        HttpStatus.INTERNAL_SERVER_ERROR,
+                    );
                 }
+
+                const user = await this.userModel.create({
+                    sudoID: sudoCustomerWallet.data.data._id,
+                    customerType: sudoCustomerWallet.data.data.type,
+                    status: sudoCustomerWallet.data.data.status,
+                    email: sudoCustomerWallet.data.data.emailAddress,
+                    password: hashedPassword,
+                    fullName: sudoCustomerWallet.data.data.name,
+                    phoneNumber: sudoCustomerWallet.data.data?.phoneNumber,
+                    address:
+                        sudoCustomerWallet.data.data?.billingAddress?.line1,
+                    city: sudoCustomerWallet.data.data?.billingAddress?.city,
+                    state: sudoCustomerWallet.data.data?.billingAddress?.state,
+                    postalCode:
+                        sudoCustomerWallet.data.data?.billingAddress
+                            ?.postalCode,
+                    country:
+                        sudoCustomerWallet.data.data?.billingAddress?.country,
+                    firstName:
+                        sudoCustomerWallet.data.data?.individual?.firstName,
+                    lastName:
+                        sudoCustomerWallet.data.data?.individual?.lastName,
+                    dob: sudoCustomerWallet.data.data?.individual?.dob,
+                    identityType:
+                        sudoCustomerWallet.data.data?.individual?.identity
+                            ?.type,
+                    identityNumber:
+                        sudoCustomerWallet.data.data?.individual?.identity
+                            ?.number,
+                    companyName: sudoCustomerWallet.data.data?.company?.name,
+                    companIdentityType:
+                        sudoCustomerWallet.data.data?.company?.identity?.type,
+                    companyIdentityNumber:
+                        sudoCustomerWallet.data.data?.company?.identity?.number,
+                });
+                return user;
             }
-
-            // console.log(data)
-
-
-            const options = {
-                method: 'POST',
-                url: url,
-                headers: this.headers,
-                data: data
-            }
-                    
-            const response = await axios.request(options);
-
-            const user = await this.userModel.create({
-                sudoID: response.data.data._id,
-                customerType : response.data.data.type,
-                status: response.data.data.status,
-                email: response.data.data.emailAddress,
-                password: hashedPassword,
-                fullName: response.data.data.name,
-                phoneNumber: response.data.data?.phoneNumber,
-                address: response.data.data?.billingAddress?.line1,
-                city: response.data.data?.billingAddress?.city,
-                state: response.data.data?.billingAddress?.state,
-                postalCode: response.data.data?.billingAddress?.postalCode,
-                country: response.data.data?.billingAddress?.country,
-                firstName: response.data.data?.individual?.firstName,
-                lastName: response.data.data?.individual?.lastName,
-                dob: response.data.data?.individual?.dob,
-                identityType: response.data.data?.individual?.identity?.type,
-                identityNumber: response.data.data?.individual?.identity?.number,
-                companyName: response.data.data?.company?.name,
-                companIdentityType: response.data.data?.company?.identity?.type ,
-                companyIdentityNumber: response.data.data?.company?.identity?.number,
-            })
-            return user
-
         } catch (err) {
             if (err?.code === MongoError.DuplicateKey) {
                 throw new HttpException(
                     'User with that email already exists',
-                    HttpStatus.BAD_REQUEST
-                )
+                    HttpStatus.BAD_REQUEST,
+                );
             }
+
+            console.log(err, err.response.data, err.response.data);
             throw new HttpException(
                 'Something went wrong while creating an account, Try again!',
-                HttpStatus.INTERNAL_SERVER_ERROR
-            )
+                HttpStatus.INTERNAL_SERVER_ERROR,
+            );
         }
     }
 
-
-
-
     async delete(sudoID: string) {
-        const user = await this.userModel.findOne({ sudoID: sudoID })
+        const user = await this.userModel.findOne({ sudoID: sudoID });
 
         if (!user) {
-            throw new HttpException('User with this id does not exist', HttpStatus.NOT_FOUND);
-        } 
+            throw new HttpException(
+                'User with this id does not exist',
+                HttpStatus.NOT_FOUND,
+            );
+        }
 
-        await user.delete()
+        await user.delete();
     }
-
-   
-
-
 }
